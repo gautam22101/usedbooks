6d38a42e18f3466039532bb9f27c93ae2801775c
gautam22101 <153732306+gautam22101@users.noreply.github.com>
2023-12-13 18:08:13 +0530

    added before login files

---
+++ beforelogin.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+
+namespace usedbooks
+{
+    public partial class default2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+       
+        protected void RedirectToProductInfo(object sender, EventArgs e)
+        {
+            // Redirect to the "productinfo" web form
+            Response.Redirect("productinfo.aspx");
+        }
+       
+
+
+    }
+}
